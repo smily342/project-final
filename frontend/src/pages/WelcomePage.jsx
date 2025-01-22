e1dbc4fb94d6505453abd3274a4eaee2a06ea001
smily342 <B_dadi@ik.me>
2025-01-22 19:38:37 +0100

    params comment

--- frontend/src/pages/WelcomePage.jsx
+++ frontend/src/pages/WelcomePage.jsx
@@ -1,4 +1,4 @@
-import { useParams, Link } from "react-router-dom";
+// import { useParams, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { FeaturedBooks } from "../components/FeaturedBooks";
 import { HeaderOne } from "../components/HeaderOne";
