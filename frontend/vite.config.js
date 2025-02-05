c556eac31fa7ea2420ce107b307eb82caeee99d3
Heléne Abrahamsson <helene.abrahamsson91@gmail.com>
2025-02-05 22:06:25 +0100

    fixed my page and library page

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure the base path is correct
   server: {
     proxy: {
-       "/genres": {
-        target: 'http://localhost:3000', // Backend server
+      "/genres": {
+        target: 'https://project-final-044d.onrender.com', // Updated backend server
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
